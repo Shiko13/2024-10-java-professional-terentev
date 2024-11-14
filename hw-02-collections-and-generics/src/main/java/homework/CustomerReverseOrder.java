bda910d76362f5bd5c83e5cb3fe1d0f3b47d1d17
Sergei Terentev <s.terentev@iteco-inno.ru>
2024-11-14 09:57:27 +0300

    Fix after review

--- hw-02-collections-and-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw-02-collections-and-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,10 +1,11 @@
 package homework;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class CustomerReverseOrder {
 
-    LinkedList<Customer> customers = new LinkedList<>();
+    List<Customer> customers = new LinkedList<>();
 
     public void add(Customer customer) {
         customers.add(customer);
