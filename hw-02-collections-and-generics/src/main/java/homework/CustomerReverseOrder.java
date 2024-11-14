835d99b3bd4374dc33f8f4f2d3f1837be096497c
Sergei Terentev <s.terentev@iteco-inno.ru>
2024-11-14 11:06:52 +0300

    Fix after review

--- hw-02-collections-and-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw-02-collections-and-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,11 +1,11 @@
 package homework;
 
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 
 public class CustomerReverseOrder {
 
-    List<Customer> customers = new LinkedList<>();
+    private final Deque<Customer> customers = new LinkedList<>();
 
     public void add(Customer customer) {
         customers.add(customer);
