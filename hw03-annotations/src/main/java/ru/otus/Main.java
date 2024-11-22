16d69d303e213ac62f80d2254c47fae5bbffd1ff
Sergei Terentev <s.terentev@iteco-inno.ru>
2024-11-22 17:18:03 +0300

    Add statistics

--- hw03-annotations/src/main/java/ru/otus/Main.java
+++ hw03-annotations/src/main/java/ru/otus/Main.java
@@ -4,76 +4,84 @@ import ru.otus.annotations.After;
 import ru.otus.annotations.Before;
 import ru.otus.annotations.Test;
 import ru.otus.test.TestClass;
+import ru.otus.test.TestResult;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    private static Method beforeMethod;
-    private static Method afterMethod;
-    private static final List<Method> testMethods = new ArrayList<>();
-
     public static void main(String[] args) {
-        Class<?> testClass = TestClass.class;
-        runTests(testClass);
-    }
+        TestClass testClass = new TestClass();
+        TestResult testResult = new TestResult();
 
-    public static void runTests(Class<?> testClass) {
-        separateToTypes(testClass);
+        Method beforeMethod = findBeforeMethod(testClass);
+        Method afterMethod = findAfterMethod(testClass);
+        List<Method> testMethods = findTestMethods(testClass);
 
-        for (Method testMethod : testMethods) {
-            System.out.println("Running test: " + testMethod.getName());
-
-            try {
-                Object testInstance = testClass.getDeclaredConstructor().newInstance();
-
-                if (beforeMethod != null) {
-                    runMethod(beforeMethod, testInstance);
-                }
-                runMethod(testMethod, testInstance);
-            } catch (Exception e) {
-                System.out.println("Test " + testMethod.getName() + " failed with error: " + e.getCause());
-            } finally {
-                try {
-                    if (afterMethod != null) {
-                        Object testInstance = testClass.getDeclaredConstructor().newInstance();
-                        runMethod(afterMethod, testInstance);
-                    }
-                } catch (Exception e) {
-                    System.out.println("After method failed with error: " + e.getCause());
-                }
-            }
-            System.out.println("Finished test: " + testMethod.getName() + "\n");
+        for (Method method : testMethods) {
+            runTest(beforeMethod, afterMethod, method, testClass, testResult);
         }
+
+        printTestStatistics(testResult);
     }
 
-    private static void separateToTypes(Class<?> testClass) {
-        for (Method method : testClass.getMethods()) {
+    private static Method findBeforeMethod(TestClass testClass) {
+        for (Method method : testClass.getClass().getMethods()) {
+            if (method.isAnnotationPresent(Before.class)) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    private static Method findAfterMethod(TestClass testClass) {
+        for (Method method : testClass.getClass().getMethods()) {
+            if (method.isAnnotationPresent(After.class)) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    private static List<Method> findTestMethods(TestClass testClass) {
+        List<Method> testMethods = new ArrayList<>();
+        for (Method method : testClass.getClass().getMethods()) {
             if (method.isAnnotationPresent(Test.class)) {
                 testMethods.add(method);
-            } else if (method.isAnnotationPresent(Before.class)) {
-                if (beforeMethod != null) {
-                    throw new IllegalStateException("Multiple @Before methods are not allowed");
-                }
-                beforeMethod = method;
-            } else if (method.isAnnotationPresent(After.class)) {
-                if (afterMethod != null) {
-                    throw new IllegalStateException("Multiple @After methods are not allowed");
-                }
-                afterMethod = method;
             }
         }
+        return testMethods;
+    }
 
-        if (testMethods.isEmpty()) {
-            System.out.println("No test methods found in class: " + testClass.getName());
+    private static void runTest(Method beforeMethod, Method afterMethod, Method testMethod,
+                                TestClass testClass, TestResult testResult) {
+        try {
+            if (beforeMethod != null) {
+                beforeMethod.invoke(testClass);
+            }
+
+            testMethod.invoke(testClass);
+            testResult.incrementPassedTests();
+        } catch (Exception e) {
+            testResult.incrementFailedTests();
+            System.out.println("Test " + testMethod.getName() + " failed with exception: " + e.getMessage());
+        } finally {
+            if (afterMethod != null) {
+                try {
+                    afterMethod.invoke(testClass);
+                } catch (Exception e) {
+                    System.err.println("After method failed with exception: " + e.getMessage());
+                }
+            }
         }
     }
 
-    private static void runMethod(Method method, Object testInstance) throws InvocationTargetException, IllegalAccessException {
-        System.out.println("Executing method: " + method.getName());
-        method.invoke(testInstance);
+    private static void printTestStatistics(TestResult testResult) {
+        System.out.println("Test statistics:");
+        System.out.println("Total tests run: " + testResult.getTotalTests());
+        System.out.println("Passed tests: " + testResult.getPassedTests());
+        System.out.println("Failed tests: " + testResult.getFailedTests());
     }
 }
--- hw03-annotations/src/main/java/ru/otus/test/TestClass.java
+++ hw03-annotations/src/main/java/ru/otus/test/TestClass.java
@@ -9,37 +9,61 @@ import ru.otus.annotations.Test;
 public class TestClass {
 
     private static final Logger log = LoggerFactory.getLogger(TestClass.class);
-    private int counter = 0;
 
     @Before
     public void setup() {
-        counter = 10;
-        log.info("Before method: Counter initialized to {}", counter);
+        log.info("Before method: Test setup.");
     }
 
     @Test
-    public void testIncrement() {
-        log.info("Test method: Incrementing counter");
-        counter++;
-        log.info("Test method: Counter incremented to {}", counter);
-        if (counter != 11) {
-            throw new AssertionError("Counter expected to be 11 but was " + counter);
+    public void testAddition() {
+        log.info("Test method: Testing addition operation.");
+        int a = 5;
+        int b = 3;
+        int result = a + b;
+        int expected = 8;
+
+        if (result != expected) {
+            throw new AssertionError("Test failed: Expected " + expected + " but got " + result);
+        } else {
+            log.info("Test passed: Addition was correct.");
         }
     }
 
     @Test
-    public void testDecrement() {
-        log.info("Test method: Decrementing counter");
-        counter--;
-        log.info("Test method: Counter decremented to {}", counter);
-        if (counter != 9) {
-            throw new AssertionError("Counter expected to be 9 but was " + counter);
+    public void testSubtraction() {
+        log.info("Test method: Testing subtraction operation.");
+        int a = 5;
+        int b = 3;
+        int result = a - b;
+        int expected = 2;
+
+        if (result != expected) {
+            throw new AssertionError("Test failed: Expected " + expected + " but got " + result);
+        } else {
+            log.info("Test passed: Subtraction was correct.");
+        }
+    }
+
+    @Test
+    public void testErrorScenario() {
+        log.info("Test method: Testing an error scenario.");
+
+        int a = 5;
+        int b = 3;
+        int result = a + b;
+        int expected = 12;
+
+        // Проверка
+        if (result != expected) {
+            throw new AssertionError("Test failed: Expected " + expected + " but got " + result);
+        } else {
+            log.info("Test passed: The error scenario was handled correctly.");
         }
     }
 
     @After
     public void tearDown() {
-        log.info("After method: Counter reset to 0");
-        counter = 0;
+        log.info("After method: Test cleanup.");
     }
 }
