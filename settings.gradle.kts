ab83324dddaca29e74016a934119ff2a28970c64
Sergei Terentev <s.terentev@iteco-inno.ru>
2024-11-28 14:25:34 +0300

    Add initial HW04 code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,9 @@
 rootProject.name = "otusJava"
 include("hw01-gradle")
 include("hw02-collections-and-generics")
-include("hw03-annotations")
+include("hw04-garbage-collector")
 
+include("hw03-annotations")
 pluginManagement {
     val dependencyManagement: String by settings
     val springframeworkBoot: String by settings
