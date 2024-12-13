3bed759a49d6c5a8022996d1387fa0ae3c59ac74
Sergei Terentev <s.terentev@iteco-inno.ru>
2024-12-13 21:44:26 +0300

    Add homework06

--- hw06-oop/src/main/java/ru/otus/Main.java
+++ hw06-oop/src/main/java/ru/otus/Main.java
@@ -1,7 +1,28 @@
 package ru.otus;
 
+import ru.otus.domain.Atm;
+import ru.otus.domain.Denomination;
+import ru.otus.exception.TransactionException;
+
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Atm atm = new Atm();
+
+        atm.deposit(Denomination.HUNDRED, 10);
+        atm.deposit(Denomination.FIFTY, 20);
+        atm.deposit(Denomination.TEN, 100);
+        System.out.println("Текущий баланс: " + atm.getBalance());
+
+        try {
+            Map<Denomination, Integer> withdrawn = atm.withdraw(580);
+            System.out.println("Выданные купюры: " + withdrawn);
+        } catch (TransactionException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        System.out.println("Текущий баланс после снятия: " + atm.getBalance());
+        System.out.println("Состояние хранилища: " + atm.getStorageState());
     }
 }
