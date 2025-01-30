69fd652c5e717d0c9214ea4bf91d36cca67950d2
Sergei Terentev <s.terentev@iteco-inno.ru>
2025-01-30 12:29:13 +0300

    Add hw10

--- hw10-jpql/src/main/java/ru/otus/crm/model/Address.java
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Address.java
@@ -1,7 +1,27 @@
 package ru.otus.crm.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "address")
+@EqualsAndHashCode(callSuper = false)
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+
     public Address(Long id, String street) {
-        throw new UnsupportedOperationException();
+        this.id = id;
+        this.street = street;
     }
 }
