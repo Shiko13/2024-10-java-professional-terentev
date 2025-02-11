f66551c49f179722535ce98bb78f54acc9f3c0e5
Sergei Terentev <s.terentev@iteco-inno.ru>
2025-02-11 16:46:41 +0300

    Add hw13

--- hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,12 +1,12 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static ru.otus.utils.ReflectionUtils.*;
 
 @SuppressWarnings("squid:S1068")
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -18,10 +18,55 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         processConfig(initialConfigClass);
     }
 
+    @Override
+    public <C> C getAppComponent(Class<C> componentClass) {
+        List<Object> matchingComponents = appComponents.stream()
+                .filter(component -> componentClass.isAssignableFrom(component.getClass()))
+                .toList();
+
+        if (matchingComponents.isEmpty()) {
+            throw new RuntimeException(String.format("Компонент типа %s не найден", componentClass));
+        }
+        if (matchingComponents.size() > 1) {
+            throw new RuntimeException(String.format("Обнаружены несколько реализаций для %s", componentClass));
+        }
+        return (C) matchingComponents.get(0);
+    }
+
+    @Override
+    public <C> C getAppComponent(String componentName) {
+        Object component = appComponentsByName.get(componentName);
+        if (component == null) {
+            throw new RuntimeException(String.format("Компонент с именем '%s' не найден", componentName));
+        }
+        return (C) component;
+    }
+
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+        findAnnotatedMethods(configClass, AppComponent.class).stream()
+                .sorted(Comparator.comparingInt(method -> method.getAnnotation(AppComponent.class).order()))
+                .forEach(method -> {
+                    var componentName = method.getAnnotation(AppComponent.class).name();
+                    var componentInstance = callMethod(createInstance(configClass), method, resolveDependencies(method.getParameterTypes()));
+                    registerComponent(componentName, componentInstance);
+                });
     }
+
+    private Object[] resolveDependencies(Class<?>[] parameterTypes) {
+        return Arrays.stream(parameterTypes)
+                .map(this::getAppComponent)
+                .toArray();
+    }
+
+    private void registerComponent(String componentName, Object componentInstance) {
+        if (isComponentExists(componentName)) {
+            throw new RuntimeException(String.format("Имя %s уже занято", componentName));
+        }
+        appComponentsByName.put(componentName, componentInstance);
+        appComponents.add(componentInstance);
+    }
+
 
     private void checkConfigClass(Class<?> configClass) {
         if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
@@ -29,13 +74,7 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         }
     }
 
-    @Override
-    public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
-    }
-
-    @Override
-    public <C> C getAppComponent(String componentName) {
-        return null;
+    private boolean isComponentExists(String componentName) {
+        return appComponentsByName.containsKey(componentName);
     }
 }
